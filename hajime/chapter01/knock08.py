074138cfd6a1a977efff107944f67ca3006949e5
kiyama-hajime <hajime.7393@gmail.com>
2022-04-22 14:17:15 +0900

    replace knock08.py

--- hajime/chapter01/knock08.py
+++ hajime/chapter01/knock08.py
@@ -8,8 +8,11 @@ def cipher(trg_str):
         if trg_str[i].islower():
             small = ord(trg_str[i])
             output += chr(219 - small)
+        else:
+            output += trg_str[i]
     return output
             
 trg_str = sys.argv[1]
 
 print(cipher(trg_str))
+print(cipher(cipher(trg_str)))
