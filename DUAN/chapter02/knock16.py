df2cad972e9866161c70322a8b4b661db8d6aee1
DUAN‐2022 <87805036+standalone2021@users.noreply.github.com>
2022-05-06 11:11:26 +0900

    Update knock16.py

--- DUAN/chapter02/knock16.py
+++ DUAN/chapter02/knock16.py
@@ -1,14 +1,11 @@
 import math
 
-n = 5 # Nを取得する
+n = int(sys.argv[1]) # nを取得する
+lines = open('./100knock2022/DUAN/chapter02/popular-names.txt').readlines() # ファイルを読み込む
+count = len(lines) # 長さを計算する
+unit = math.ceil(count / n)  # 一つのファイルの行数
 
-# ファイルの読み込み
-lines = open('./100knock2022/DUAN/chapter02/popular-names.txt').readlines()
-
-count = len(lines)
-unit = math.ceil(count / n)  # 1ファイル当たりの行数
-
-for i, j in enumerate(range(0, count, unit), 1):
-    with open('child_{:02d}.txt'.format(i), mode='w') as out_file:
+for i, j in enumerate(range(0, count, unit), 1): # インデックスと要素を同時に取り出す
+    with open('out_{:02d}.txt'.format(i), mode='w') as out: # 書式を指定して、出力ファイルを作る
         for line in lines[j:j + unit]:
-            out_file.write(line)
+            out.write(line) # ファイルへの出力
