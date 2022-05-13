a476df0b5c1c2cf2135fd0e184ba6416f9a0b43b
DUAN‐2022 <87805036+standalone2021@users.noreply.github.com>
2022-05-13 10:44:19 +0900

    Update knock20.py

--- DUAN/chapter03/knock20.py
+++ DUAN/chapter03/knock20.py
@@ -1,8 +1,7 @@
 import pandas as pd
  
-filename = "jawiki-country.json"
-j_data = pd.read_json(filename, lines =True)
-df = j_data
-uk_df = df[df["title"]=="イギリス"]
-uk_df = uk_df["text"].values
-print(uk_df)
+name = "jawiki-country.json"
+data = pd.read_json(name, lines = True)
+answ = data[data["title"]=="イギリス"]
+an = answ["text"].values
+print(an)
