2579cc196c7587f027abe56e7978dbd08b535679
kiyama-hajime <hajime.7393@gmail.com>
2022-05-13 09:53:42 +0900

    update 24 explain

--- hajime/chapter03/knock24.py
+++ hajime/chapter03/knock24.py
@@ -4,15 +4,18 @@ import json
 import gzip
 import re
 
+# イギリスの記事を獲得
 with gzip.open('jawiki-country.json.gz', 'rt') as f:
     for line in f:
-        content = json.loads(line) # type-str so s is added
-        if ("title","イギリス") in content.items():
+        content = json.loads(line)  # type-str so s is added
+        if ("title", "イギリス") in content.items():
             eng = content["text"]
 
 eng_list = eng.split("\n")
 for line in eng_list:
-    if re.match(r'^\[\[ファイル:(.*)\]\]$',line):
-        file = re.findall(r'^\[\[ファイル:(.*)\]\]$',line)
+    # [[ファイル:hoge.png|thumb|説明文]]となる部分を調査
+    if re.match(r'^\[\[ファイル:(.*)\]\]$', line):
+        # 'ファイル:hoge.png|thumb|説明文'を変数fileに格納
+        file = re.findall(r'^\[\[ファイル:(.*)\]\]$', line)
         trg_file = file[0].split('|')
         print(trg_file[0])
