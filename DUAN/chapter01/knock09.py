22b6f79ed527f2395d17e44cad9d755cf309046c
DUAN‐2022 <87805036+standalone2021@users.noreply.github.com>
2022-05-20 17:07:03 +0900

    Update knock09.py

--- DUAN/chapter01/knock09.py
+++ DUAN/chapter01/knock09.py
@@ -1,16 +1,16 @@
 import random
 
-def Typoglycemia(sentence):
-    a = sentence.split(' ')
+def Typoglycemia(sen):
+    a = sen.split(' ')
     b = list()
     for i in a:
         if len(i) <= 4:
             b.append(i)
         else:
-            temp = i[1:len(x)-1]
-            temp = ''.join(random.sample(temp,len(temp)))
-            b.append(i[0] + temp + i[len(i)-1])
+            c = i[1:len(x)-1]
+            c = ''.join(random.sample(c,len(c)))
+            b.append(i[0] + c + i[len(i)-1])
     return ' '.join(map(str,b))
 
-x = r"I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+x = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
 print(Typoglycemia(x))
