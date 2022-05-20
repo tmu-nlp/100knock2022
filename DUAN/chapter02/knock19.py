ff8d8e70b6da14441ec98c00b633c3e5fb753225
DUAN‐2022 <87805036+standalone2021@users.noreply.github.com>
2022-05-20 17:09:56 +0900

    Update knock19.py

--- DUAN/chapter02/knock19.py
+++ DUAN/chapter02/knock19.py
@@ -1,7 +1,7 @@
 lines = open('./100knock2022/DUAN/chapter02/popular-names.txt').readlines()
 col1_count = dict()
 for l in lines:
-    c = l.split("\t")[0]
+    c = l.split('\t')[0]
     if col1_count.get(c):
         col1_count[c]+=1
     else:
