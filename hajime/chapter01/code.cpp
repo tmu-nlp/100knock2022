fe7598458d06932788c2c965de5af5a84d65a5d6
kiyama-hajime <hajime.7393@gmail.com>
2022-05-23 16:31:55 +0900

    test

---
+++ hajime/chapter01/code.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
