ce00ee17afc42bc2cb97860682946342a0c9bb2d
kiyama-hajime <hajime.7393@gmail.com>
2022-05-27 12:52:14 +0900

    update chapter07

--- hajime/chapter07/knock65.py
+++ hajime/chapter07/knock65.py
@@ -1,15 +1,30 @@
 
-length = 0
-cnt = 0
-with open("64.txt", "r") as t_file:
+length1 = 0
+cnt1 = 0
+with open("64-semantic.txt", "r") as t_file:
     for line in t_file:
-        length += 1
+        length1 += 1
         line_list = line.strip().split(" ")
         if line_list[3] == line_list[4]:
-            cnt += 1
+            cnt1 += 1
 
-print(float(cnt)/length)
+print("semantic analogy")
+print(float(cnt1)/length1)
+
+length2 = 0
+cnt2 = 0
+with open("64-syntactic.txt", "r") as t_file:
+    for line in t_file:
+        length2 += 1
+        line_list = line.strip().split(" ")
+        if line_list[3] == line_list[4]:
+            cnt2 += 1
+print("syntactic analogy")
+print(float(cnt2)/length2)
 
 """
-0.7358780188293083
+semantic analogy
+0.7308602999210734
+syntactic analogy
+0.7400468384074942
 """
