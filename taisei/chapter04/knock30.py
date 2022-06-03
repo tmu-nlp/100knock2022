f845e5eb1962b40c64fcc4e9f2cfd6adf3a791cc
enomooon <enomoto-taisei@ed.tmu.ac.jp>
2022-06-03 15:03:44 +0900

    add chapter07

--- taisei/chapter04/knock30.py
+++ taisei/chapter04/knock30.py
@@ -10,7 +10,10 @@ morpheme_line_list = []
 for line in data: 
     #line = line.strip()をすると文頭のスペース（段落）を消しちゃうから今回はしない
     rs = re.match(r'(.*?)\t(.*?)\,(.*?)\,(.*?)\,(.*?)\,(.*?)\,(.*?)\,(.*?)\,(.*?)\,(.*?)', line)
+    rs2 = re.match(r'(.*?)\t(.*?)\,(.*?)\,(.*?)\,(.*?)\,(.*?)\,(.*?)', line)
     rs_eos = re.match(r'EOS', line)
+    """
+    これだとマッチしないものもあることに気づいた（e.g. ぷうぷうと）
     if rs:
         morpheme_dict["表層形"] = rs.group(1)
         morpheme_dict["基本形"] = rs.group(8)
@@ -18,11 +21,22 @@ for line in data:
         morpheme_dict["品詞細分類1"] = rs.group(3)
         morpheme_line.append(morpheme_dict)
         morpheme_dict = {}
+    """
+    if rs2:
+
+        line = line.split('\t')
+        morpheme_dict["表層形"] = line[0]
+        mor = line[1].split(',')
+        morpheme_dict["基本形"] = mor[6]
+        morpheme_dict["品詞"] = mor[0]
+        morpheme_dict["品詞細い分類1"] = mor[1]
+        morpheme_line.append(morpheme_dict)
+        morpheme_dict = {}
 
     elif rs_eos:
         morpheme_line_list.append(morpheme_line)
         morpheme_line = []
 
-with open("knock30_output.txt", "w") as f_out:
+with open("./output/knock30_output.txt", "w") as f_out:
     for x in morpheme_line_list:
         f_out.write(f'{x}\n')
