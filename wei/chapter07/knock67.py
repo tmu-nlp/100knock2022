41c26ba3735a749ef2ac58241001ada7d793ad51
houjing.wei <weihoujing@163.com>
2022-06-10 11:28:40 +0900

    add to chapter07

--- wei/chapter07/knock67.py
+++ wei/chapter07/knock67.py
@@ -38,49 +38,48 @@ if __name__ == '__main__':
     start = datetime.datetime.now()
     print(f'start running time: {start}.')
 
-    wv = load_wv()
+    wv = load_wv()    # load KeyedVectors
     with open('../data/questions-words.txt','r', encoding='utf-8') as f1:
         lines = f1.readlines()
-
     countries_1 = get_country_name(lines, 'capital-common-countries')
     countries_2 = get_country_name(lines, 'capital-world')
     countries_3 = get_country_name(lines, 'currency')
-
     final_countries = list(set(countries_1 + countries_2 + countries_3))
-
-    end = datetime.datetime.now()
-    print(f'end running time: {end}.')
-    print(f'running time : {end-start}.')
 
     # 国名のベクトルを取得
     vec_ctrs = [wv[country] for country in final_countries]
-
     vec_ctrs_arr = np.array(vec_ctrs)
     # print(vec_ctrs_arr.shape)   (144, 300)
 
+    # k-means clustering
+    # fit(X,y=None):
+    # y:ignored, present for API consistency, compute k-means clustering, return fitted estimator object.
+    # fit_predict(X,y):
+    # compute cluster centers and predict cluster index for each sample.
+    # return ndarray of shape (n_samples,):index of cluster each sample belongs to.
 
-    y_pred = KMeans(n_clusters=5, random_state=112).fit_predict(vec_ctrs_arr)
-    plt.scatter(vec_ctrs_arr[:,0], vec_ctrs_arr[:,1], c=y_pred)    # all rows in 1st column, all rows in 2nd column, not ok if ndarray(n>=2)
-    plt.show()
-    plt.savefig('./knock67.jpg')
+    y_pred = KMeans(n_clusters=5, random_state=112).fit(vec_ctrs_arr)
+    for i in range(5):
+         cluster = np.where(y_pred.labels_== i)[0]
+         print('cluster', i)
+         print(','.join([final_countries[k] for k in cluster]))
 
-
-    np.save('./vec_ctrs_arr.npy', vec_ctrs_arr)
-    with open('./total_num_ctrs.txt', 'wb') as f2:
-         pickle.dump(final_countries, f2)
-
-
-
-
-
-
-
-
-
-
+    # all rows in 1st column, all rows in 2nd column, not ok if ndarray(n>=2)
+    # plt.scatter(vec_ctrs_arr[:,0], vec_ctrs_arr[:,1], c=y_pred.labels_)
+    #
+    # plt.show()
+    # plt.savefig('./knock67_fit.jpg')
+    #
+    #
+    # np.save('./vec_ctrs_arr.npy', vec_ctrs_arr)
+    # with open('./total_num_ctrs.txt', 'wb') as f2:
+    #     pickle.dump(final_countries, f2)   #持久化包括python自定义在内的各种数据，将python对象保存
 
 
 
+    end = datetime.datetime.now()
+    print(f'end running time: {end}.')
+    print(f'running time : {end-start} seconds.')
 
 
 
