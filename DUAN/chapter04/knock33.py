e1128c03236e9f838a2d65affb875cb5bfca84ba
DUAN‐2022 <87805036+standalone2021@users.noreply.github.com>
2022-07-04 16:52:35 +0900

    Update knock33.py

--- DUAN/chapter04/knock33.py
+++ DUAN/chapter04/knock33.py
@@ -4,9 +4,12 @@ f = open('./100knock2022/DUAN/chapter04/neko.txt.mecab')
 for line in f:  
     if line != 'EOS\n': 
         fields = line.split('\t')
-        attr =  fields[1].split(',')
-        morph = {'surface': fields[0], 'base': attr[6], 'pos': attr[0], 'pos1': attr[1]}
-        mors.append(morph)
+        if len(fields) != 2 or fields[0] == '':
+            continue
+        else:
+            attr =  fields[1].split(',')
+            morph = {'surface': fields[0], 'base': attr[6], 'pos': attr[0], 'pos1': attr[1]}
+            mors.append(morph)
     else:  
         sen.append(mors)
         mors = []
