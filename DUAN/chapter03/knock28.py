79fcf95df7b8bcda67ded54b34b31b9428a8b3ed
DUAN‐2022 <87805036+standalone2021@users.noreply.github.com>
2022-07-04 16:49:15 +0900

    Update knock28.py

--- DUAN/chapter03/knock28.py
+++ DUAN/chapter03/knock28.py
@@ -15,7 +15,7 @@ for t in an[0].split('\n'): # 要素を改行文字で区切る
         d[tem[1]] = tem[2] # キーと値を指定し、dに追加する
     match = re.sub("\'{2,}(.+?)\'{2,}", '\\1', t) # 文字列置換
     match2 = re.sub('\[\[(.+?)\]\]', '\\1', match)
-    match3= re.sub('\[(.+?)\]', '\\1', match2) 
+    match3 = re.sub('\[(.+?)\]', '\\1', match2) 
     match4 = re.sub('\*+(.+?)', '\\1', match3) 
     match5 = re.sub('\:+(.+?)', '\\1', match4)
     match6 = re.sub('\{\{(.+?)\}\}', '\\1', match5) 
