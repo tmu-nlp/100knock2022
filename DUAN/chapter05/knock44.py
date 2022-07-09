e1d136de340641aac1c9d191a0c937b0a0719f12
DUAN‐2022 <87805036+standalone2021@users.noreply.github.com>
2022-07-09 17:22:30 +0900

    Update knock44.py

--- DUAN/chapter05/knock44.py
+++ DUAN/chapter05/knock44.py
@@ -1,75 +1,80 @@
-from graphviz import Digraph
-dg=Digraph(format='png')
+import re
+import pydot
+from IPython.display import Image
 
-class Morph:
-    def __init__(self,surface,base,pos,pos1):
-        self.surface=surface
-        self.base=base
-        self.pos=pos
-        self.pos1=pos1
+class Morph(object):
+    srcs = -1
+    def __init__(self, surface, base, pos, pos1):
+        self.surface = surface
+        self.base = base
+        self.pos = pos
+        self.pos1 = pos1
 
-    def __str__(self):
-        x="surface:{0}\tbase:{1}\tpos:{2}\tpos1:{3}"
-        return x.format(self.surface,self.base,self.pos,self.pos1)
+class Chunk(object):
+    def __init__(self, morphs, dst, srcs):
+        self.morphs = morphs
+        self.dst = dst
+        self.srcs = srcs
 
-class Chunk:
-    def __init__(self):
-        self.counter=0
-        self.morphs=[]
-        self.dst=-1
-        self.srcs=[]
+    def get_surface(self):
+        surface = [morph.surface for morph in self.morphs]
+        surface = ('').join(surface)
+        return re.sub(r'[、。]', '', surface)
 
-morph=[]
-prin=0
-count=0
-chunks=dict()
-chunks[count]=Chunk()
-with open('./100knock2022/DUAN/chapter05/ai.ja.txt.parsed',encoding='utf-8')as f:
-    for line in f:
-        surface=line.split('\t')
-        chunks[count+1]=Chunk()
-        if len(surface)>1:
-            other=surface[1].split(',')
-            Base=other[6]
-            Pos=other[0]
-            Pos1=other[1]
-            morph.append(Morph(surface[0],Base,Pos,Pos1))
-            if Pos!='記号':
-                chunks[count].morphs.append(surface[0])#chunk.morphs
-        elif 'EOS' not in line:
-            kakari=line.split(' ')
-            count=int(kakari[1])
-            dst=kakari[2].replace('D','')
-            chunks[count].dst=int(dst)                    #chunk.dst
-            chunks[count].counter=count
-        else:
-            for number in range(count+1):
-                if chunks[number].dst != -1:
-                    chunks[chunks[number].dst].srcs.append(number)
-                dg.node(''.join(chunks[number].morphs))
+    def get_pos(self, pos):
+        return [morph for morph in self.morphs
+                if morph.pos == pos]
 
-            for result in range(count+1):
-                x="{0}{1}\t{2}{3}"
-                if chunks[result].dst == -1:
-                    saki=''
-                else:
-                    saki=''.join(chunks[chunks[result].dst].morphs)
-                if saki:
-                    moto=''.join(chunks[result].morphs)
-                    dg.edge(moto,saki)
-            if dg:
-                prin+=1
-                filename='nock44'+str(prin)
-                dg.render(filename)
-                dg.clear()
+    def apply_index(self, index):
+        for morph in self.morphs:
+            morph.chunk_id = index
 
-            for ketu in range(count+1):
-                chunks[ketu].morphs=[]
-                chunks[ketu].srcs=[]
-            count=0
+    def has_pos(self, pos):
+        return pos in [morph.pos for morph in self.morphs]
 
-'''
-print(pairs)
-g = pydot.graph_from_edges(pairs)
-g.write_png('./100knock2022/DUAN/chapter05/knock44.png', prog='dot')
-'''
+    def replace_pos(self, pos, X):
+        surface = ''
+        for morph in self.morphs:
+            if morph.pos == pos:
+                surface += X
+            else:
+                surface += morph.surface
+        return re.sub(r'[、。]', '', surface)
+
+def get_chunks(text):
+    data = []
+    for sentence in re.findall(r'(\n[\s\S]*?EOS)', text):
+        chunks = []
+        for clause in re.findall(
+                r'\* (\d*) (-?\d+).*?\n([\s\S]*?)(?=\n\*|\nEOS)', sentence):  
+            morphs = []
+            srcs = re.findall(
+                r'\* (\d*) ' + clause[0] + r'D.*?\n[\s\S]*?(?=\n\*|\nEOS)',
+                sentence)
+            for line in re.findall(r'(.*?)\t(.*?)(?:$|\n)', clause[2]): 
+                surface = line[0]
+                feature = line[1].split(',')
+                morph = Morph(surface, feature[6], feature[0], feature[1])
+                morphs.append(morph)
+            chunk = Chunk(morphs, int(clause[1]), list(map(int, srcs)))
+            chunks.append(chunk)
+        data.append(chunks)
+    return data
+
+graph = pydot.Dot(graph_type='digraph')
+chunks = ''
+
+with open('./100knock2022/DUAN/chapter05/ai.ja.txt.parsed', 'r') as f:
+    text = f.read()
+    chunks = get_chunks(text)[7]
+
+for chunk in chunks:
+    if chunk.dst == -1:
+        continue
+    surface = chunk.get_surface()
+    dst_surface = chunks[chunk.dst].get_surface()
+    edge = pydot.Edge(surface, dst_surface)
+    graph.add_edge(edge)
+
+graph.write('./100knock2022/DUAN/chapter05/knock44.png', format="png")
+Image(graph.create(format='png'))
