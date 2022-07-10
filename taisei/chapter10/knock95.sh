551bd0d040d75f25f0e3bed19d7abfe3a64dfd90
enomooon <enomoto-taisei@ed.tmu.ac.jp>
2022-07-11 02:31:15 +0900

    add chapter10

---
+++ taisei/chapter10/knock95.sh
@@ -0,0 +1,4 @@
+#knock95_1.sh：訓練データから日英それぞれのsentencepieceモデルを作成　& データのサブワード化
+#knock95_2.sh：preprocessでバイナリデータに
+#konck95_3.sh：モデルの学習
+#knock95_4.sh： テスト時のbeamを変えた際のBLEUの変化をグラフに
---
+++ taisei/chapter10/knock96.sh
@@ -0,0 +1,2 @@
+#学習過程の可視化
+#knock95_3.shの"--tensorboard-logdir output/knock95/tensorboard_logdir"でtensorboard用のディレクトリを作成してる
